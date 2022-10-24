40216fce72f877cfe4f769d8811b63bf0fd5b754
Peter Harrison <pmch2@cam.ac.uk>
2022-10-24 21:24:20 +0100

    Update visualisation

--- scripts/illustrations/hutch78-model-illustrations.R
+++ scripts/illustrations/hutch78-model-illustrations.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(ggpubr)
 library(hrep)
 
+source("src/custom_hutchinson_knopoff_model.R")
+
 theme_set(theme_pubr())
 
 dissonance_curve <- function(bandwidth_distance) {
@@ -9,24 +11,22 @@ dissonance_curve <- function(bandwidth_distance) {
 }
 
 tibble(
-  cbw_dist = seq(from = 0, to = 2, by = 0.01),
-  diss = dissonance_curve(cbw_dist)
+  cbw_dist = seq(from = 0, to = 2, by = 0.001),
+  "Original model" = dissonance_curve(cbw_dist),
+  "Revised model" = revised_dissonance_factor(cbw_dist)
 ) %>% 
-  ggplot(aes(cbw_dist, diss)) + 
+  pivot_longer(cols = ends_with("model")) %>%
+  ggplot(aes(cbw_dist, value)) + 
   geom_vline(xintercept = 0.25, colour = "grey50", linetype = "dotted") +
   geom_line() + 
   scale_x_continuous("Distance between partials (critical bandwidths)") + 
-  scale_y_continuous("Dissonance factor")
+  scale_y_continuous("Dissonance factor", breaks = -1:1) + 
+  facet_wrap(~ name, ncol = 1, scales = "free_y") + 
+  theme(
+    strip.background = element_blank(),
+    panel.spacing.y = unit(20, "pt")
+  )
 
 R.utils::mkdirs("output/illustrations")
-ggsave("output/illustrations/hutchinson-knopoff-dissonance-curve.pdf", width = 5, height = 2)
-
-tibble(
-  cbw_dist = seq(from = 0, to = 2, by = 0.01),
-  diss = revised_dissonance_factor(cbw_dist)
-) %>% 
-  ggplot(aes(cbw_dist, diss)) + 
-  geom_vline(xintercept = 0.25, colour = "grey50", linetype = "dotted") +
-  geom_line() + 
-  scale_x_continuous("Distance between partials (critical bandwidths)") + 
-  scale_y_continuous("Dissonance factor")
+ggsave("output/illustrations/hutchinson-knopoff-dissonance-curve.pdf", width = 5, height = 4)
+ 
