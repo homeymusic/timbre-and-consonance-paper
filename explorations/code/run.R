66bdd13955e8a0733b50eb0a3d161230a79fb7cb
Brian Mulloy <brian@landlessness.net>
2023-02-15 08:41:29 -0500

    sweep the resolutions

--- explorations/code/run.R
+++ explorations/code/run.R
@@ -1,14 +1,4 @@
-MAMI.CODI.STRETCHED.TIBBLE = tibble::tibble(major_minor=numeric(),
-                                            consonance_dissonance=numeric(),
-                                            tonic=numeric(),
-                                            tonic_angle=numeric(),
-                                            tonic_selector=character(),
-                                            interval=integer())
-
 source("explorations/code/setup.R")
 source("explorations/code/models.R")
 source("explorations/code/create.data.R")
 # source("explorations/code/plot.R")
-
-saveRDS(MAMI.CODI.STRETCHED.TIBBLE,'/Users/landlessness/Documents/git/pmcharrison/timbre-and-consonance-paper/explorations/results/tibbles/bonang.rds')
-
